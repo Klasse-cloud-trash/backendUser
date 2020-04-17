c9d31a82064311aef643d62449684a849e04b9a1
sandra.gerberding <sandra.gerberding@cgi.com>
2020-04-17 15:03:48 +0200

    create project initial commit

---
+++ src/main/java/cloud/klasse/backenduser/BackenduserApplication.java
@@ -0,0 +1,13 @@
+package cloud.klasse.backenduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenduserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackenduserApplication.class, args);
+    }
+
+}
